31a3c01da82aa54321e50ce4d3b595a061800af3
SibirianNomad <SibirianNomad@gmail.com>
2020-08-14 09:28:13 +0700

    category edit page

--- resources/views/blog/admin/category/includes/item_edit_main_col.blade.php
+++ resources/views/blog/admin/category/includes/item_edit_main_col.blade.php
@@ -1,1 +1,63 @@
-123
+<div class='row justify-content-center'>
+    <div class='col-md-12'>
+        <div class='card'>
+            <div class='card-body'>
+            <div class='card-title'></div>
+            <ul class='nav nav-tabs' role='tablist'>
+                <li class='nav-item'>
+                    <a class='nav-link active' data-toggle='tab' href='#maindata' role='tab'>Основные данные</a>
+                </li>
+            </ul>
+            <br>
+            <div class='tab-content'>
+                <div class='tab-pave active' id='maindata' role='tabpanel'>
+                    <div class='form-group'>
+                        <label for='title'>Заголовок</label>
+                        <input name='title' value='{{ $item->title }}'
+                        id='title'
+                        type='text'
+                        class='form-control'
+                        minlength='3'
+                        required
+                        >
+                    </div>
+                    <div class='form-group'>
+                        <label for='slug'>Индентификатор</label>
+                        <input name='slug' value='{{ $item->slug }}'
+                        id='slug'
+                        type='text'
+                        class='form-control'
+                        >
+                    </div>
+                    <div class='form-group'>
+                    <label for='parent_id'>Родитель</label>
+                    <select name='parent_id'
+                    id='parent_id'
+                    class='form-control'
+                    placeholder='Выберите категорию'
+                    required
+                    >
+                    @foreach($categoryList as $category)
+                        <option value="{{ $category->id }}"
+                         @if($category->id == $item->parent_id) selected @endif>
+                        {{ $category->id }}. {{$category->title}}
+                        </option>
+                    @endforeach
+                    </select>
+                    </div>
+                    <div class='form-group'>
+                        <label for='description'>Описание</label>
+                        <textarea name='description'
+                        id='description'
+                        rows='3'
+                        class='form-control'
+                        >
+                        {{$item->description}}
+                        </textarea>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
